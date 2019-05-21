4f316e855845dc1b22fa7542e88af06c714ae7fa
PanosEvange <pan_evange@yahoo.com>
2019-05-21 20:41:07 +0300

    initial notebook and .py file

---
+++ scr_sdi1500039/sdi1500039_Project1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_markers: region,endregion
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # <center>Data Mining Project 2 Spring semester 2018-2019</center>
+# ## <center>Παναγιώτης Ευαγγελίου &emsp; 1115201500039</center>
+# ## <center>Ευάγγελος Σπίθας &emsp;&emsp;&emsp;&ensp; 1115201500147</center>
+
+# ___
+
+# ### Do all the necessary imports for this notebook
+
+import pandas as pd
