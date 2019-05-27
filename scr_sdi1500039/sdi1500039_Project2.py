faeb4ef72c3b361ab240dfa95da646dbafe7ba90
PanosEvange <pan_evange@yahoo.com>
2019-05-27 23:33:36 +0300

    question 1 done with visualizations

--- scr_sdi1500039/sdi1500039_Project2.py
+++ scr_sdi1500039/sdi1500039_Project2.py
@@ -31,33 +31,26 @@ import calendar
 # visualization
 import seaborn as sns
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 # clustering
 from sklearn.cluster import KMeans
-
 # endregion
 
 # %matplotlib inline
 
 # ## __Read data__
 
-# region
 initialDataFrame = pd.read_csv('../data/crime.csv', engine='python')
-
-# initialDataFrame # printToBeRemoved
-# endregion
 
 # ## __Do some data preparation__
 
 # region
 # drop the columns that we don't need
-processedDataFrame = initialDataFrame.drop(['Location'], axis=1)
+processedDataFrame = initialDataFrame.drop(['Location', 'OFFENSE_DESCRIPTION', 'REPORTING_AREA',
+                                            'OCCURRED_ON_DATE', 'UCR_PART', 'STREET'], axis=1)
 
 # replace some NaN values
 processedDataFrame = processedDataFrame.fillna({'SHOOTING': "N"})
-
-processedDataFrame['SHOOTING'].unique()  # printToBeRemoved
 # endregion
 
 # ## __Data Research__
@@ -66,7 +59,6 @@ processedDataFrame['SHOOTING'].unique()  # printToBeRemoved
 
 # region
 # groupBy year
-# yearCountDf = processedDataFrame.groupby(['YEAR']).count() # to be removed
 yearCountSeries = processedDataFrame.groupby(['YEAR'])['INCIDENT_NUMBER'].count()
 
 # replace month numbers to month names
@@ -75,35 +67,34 @@ monthCountDf['MONTH'] = monthCountDf['MONTH'].apply(lambda x: calendar.month_abb
 
 # groupBy month
 monthCountSeries = monthCountDf.groupby(['MONTH'])['INCIDENT_NUMBER'].count()
-# monthCountDf = monthCountDf.groupby(['MONTH']).count() # to be removed
 
 # groupBy day
-# dayCountDf = processedDataFrame.groupby(['DAY_OF_WEEK']).count() # to be removed
 dayCountSeries = processedDataFrame.groupby(['DAY_OF_WEEK'])['INCIDENT_NUMBER'].count()
 
 # groupBy district
-# districtCountDf = processedDataFrame.groupby(['DISTRICT']).count() # to be removed
 districtCountSeries = processedDataFrame.groupby(['DISTRICT'])['INCIDENT_NUMBER'].count()
-
-print("Count by year:")  # printToBeRemoved
-print(yearCountSeries)  # printToBeRemoved
-print("--------------")  # printToBeRemoved
-
-print("Count by month:")  # printToBeRemoved
-print(monthCountSeries)  # printToBeRemoved
-print("--------------")  # printToBeRemoved
-
-print("Count by day:")  # printToBeRemoved
-print(dayCountSeries)  # printToBeRemoved
-print("--------------")  # printToBeRemoved
-
-print("Count by district:")  # printToBeRemoved
-print(districtCountSeries)  # printToBeRemoved
-print("--------------")  # printToBeRemoved
-
-# for distr, count in districtCountSeries.items():
-#     print("District: ", distr, " has ", count, " crimes!")
 # endregion
+
+sns.set(style="whitegrid")
+plt.figure(figsize=(9, 9))
+ax = sns.barplot(x=yearCountSeries.index, y=yearCountSeries.values)
+ax.set(title='Count of crimes by year', xlabel='Year', ylabel='Crimes')
+plt.show()
+
+plt.figure(figsize=(9, 9))
+ax = sns.barplot(x=monthCountSeries.index, y=monthCountSeries.values)
+ax.set(title='Count of crimes by month', xlabel='Month', ylabel='Crimes')
+plt.show()
+
+plt.figure(figsize=(9, 9))
+ax = sns.barplot(x=dayCountSeries.index, y=dayCountSeries.values)
+ax.set(title='Count of crimes by day', xlabel='Day', ylabel='Crimes')
+plt.show()
+
+plt.figure(figsize=(9, 9))
+ax = sns.barplot(x=districtCountSeries.index, y=districtCountSeries.values)
+ax.set(title='Count of crimes by district', xlabel='District', ylabel='Crimes')
+plt.show()
 
 #  ### 2. Find the maximum count of shootings by year and by district
 
